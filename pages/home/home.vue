b9d8aebc9f906633f2c4fd384c51de06af36e149
Cupid Valentine <westerngod@126.com>
2022-05-19 23:43:02 +0800

    add swiper

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,16 @@
 <template>
 	<view>
-		home
+		<!-- swiper -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<swiper-item v-for="(item, i) in swiperList">
+				<view class="swiper-item">
+					<image :src="item.image_src" mode=""></image>
+				</view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item"></view>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -18,13 +28,31 @@
 		},
 		methods: {
 			async getSwiperList() {
-				const res = await uni.$http.get('/api/public/v1/home/swiperdata')
-					console.log(res)
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/swiperdata')
+				if (res.meta.status !== 200) {
+					return uni.showToast({
+						title: '数据请求失败',
+						// 1.5s
+						duration: 1500,
+						icon: 'none'
+					})
 				}
+				this.swiperList = res.message
 			}
 		}
+	}
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 330rpx;
 
+		.swiper-item,
+		image {
+			width: 100%;
+			height: 100%;
+		}
+	}
 </style>
