7e8530397240fba4e80f633ce741e5f81d0e2952
Cupid Valentine <westerngod@126.com>
2022-05-23 23:41:20 +0800

    add scroll

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,6 +1,18 @@
 <template>
 	<view>
-		cate
+		<view class="scroll-view-container">
+
+			<!-- 左侧滑动区域 -->
+			<scroll-view class="left-scroll-view" scroll-y="true" style="height: 300px;">
+
+				<view></view>
+			</scroll-view>
+			<!-- 右侧滑动区域 -->
+			<scroll-view scroll-y="true" style="height: 300px;">
+
+				<view></view>
+			</scroll-view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +20,17 @@
 	export default {
 		data() {
 			return {
-				
+
 			};
 		}
 	}
 </script>
 
 <style lang="scss">
-
+	.scroll-view-container {
+		display: flex;
+		.left-scroll-view{
+			width: 120px;
+		}
+	}
 </style>
