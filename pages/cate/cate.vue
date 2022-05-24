7ad25fd7b0b351557270ccd0e3060223e41a358e
Cupid Valentine <westerngod@126.com>
2022-05-25 00:11:10 +0800

    finished cate page

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -3,14 +3,33 @@
 		<view class="scroll-view-container">
 
 			<!-- 左侧滑动区域 -->
-			<scroll-view class="left-scroll-view" scroll-y="true" style="height: 300px;">
+			<scroll-view class="left-scroll-view" scroll-y="true" :style="{height:wh+'px'}">
+				<block v-for="(item, i) in cateList">
 
-				<view></view>
+					<view :class="['left-scroll-view-item', i===active?'active':'']" @click="activeChanged(i)">
+						{{item.cat_name}}
+					</view>
+				</block>
+
 			</scroll-view>
 			<!-- 右侧滑动区域 -->
-			<scroll-view scroll-y="true" style="height: 300px;">
+			<scroll-view scroll-y="true" :style="{height:wh+'px'}" :scroll-top="scrollTop">
 
-				<view></view>
+				<view class="cate-lv2" v-for="(item2, i2) in cateLevel2" :key="i2">
+					<!-- 二级分类标题 -->
+					<view class="cate-lv2-title">/ {{item2.cat_name}} /</view>
+					<!-- 当前二级分类下的三级分类列表 -->
+					<view class="cate-lv3-list">
+						<!-- 三级分类的item项 -->
+						<view class="cate-lv3-item" v-for="(item3, i3) in item2.children" :key="i3"
+							@click="goToGoodsList(item3)">
+							<!-- 三级分类的图片 -->
+							<image :src="item3.cat_icon"></image>
+							<!-- 三级分类的文本 -->
+							<text>{{item3.cat_name}}</text>
+						</view>
+					</view>
+				</view>
 			</scroll-view>
 		</view>
 	</view>
@@ -20,8 +39,41 @@
 	export default {
 		data() {
 			return {
+				wh: 0,
+				cateList: [],
+				cateLevel2: [],
+				active: 0,
+				scrollTop: 0
+			}
+		},
+		onLoad() {
+			const sysInfo = uni.getSystemInfoSync()
+			console.log(sysInfo)
+			this.wh = sysInfo.windowHeight
+			this.getCateList()
+		},
+		methods: {
+			async getCateList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/categories')
+				if (res.meta.status !== 200) return uni.$showMsg()
+				this.cateList = res.message
 
-			};
+				// 为二级分类赋值
+				this.cateLevel2 = res.message[0].children
+			},
+			activeChanged(i) {
+				this.active = i
+				this.cateLevel2 = this.cateList[i].children
+				this.scrollTop = this.scrollTop === 0 ? 1 : 0
+			},
+			goToGoodsList(item) {
+				console.log(item)
+				uni.navigateTo({
+					url: '/subpkg/goods_list/goods_list?cid=' + item.cat_id
+				})
+			}
 		}
 	}
 </script>
@@ -29,8 +81,63 @@
 <style lang="scss">
 	.scroll-view-container {
 		display: flex;
-		.left-scroll-view{
+
+		.left-scroll-view {
 			width: 120px;
+
+			.left-scroll-view-item {
+				position: relative;
+				background-color: #f7f7f7;
+				line-height: 60px;
+				text-align: center;
+
+				&.active {
+					background-color: #fff;
+
+					&:before {
+						position: absolute;
+						top: 50%;
+						left: 0;
+						content: ' ';
+						display: block;
+						width: 3px;
+						height: 30px;
+						background-color: #c00000;
+						transform: translateY(-50%);
+					}
+				}
+			}
+		}
+	}
+
+	.cate-lv2-title {
+		font-size: 12px;
+		font-weight: bold;
+		text-align: center;
+		padding: 15px 0;
+	}
+
+	.cate-lv3-list {
+		display: flex;
+		flex-wrap: wrap;
+
+
+		.cate-lv3-item {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			margin-bottom: 12px;
+			width: 33.3%;
+
+			image {
+				width: 60px;
+				height: 60px;
+			}
+
+			text {
+				font-size: 12px;
+			}
 		}
 	}
 </style>
